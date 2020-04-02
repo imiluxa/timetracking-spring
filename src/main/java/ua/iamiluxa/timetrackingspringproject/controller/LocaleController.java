6a6d34222eb7ab4a571cddcc88a1213ce74fc05b
iamiluxa <iamiluxa@gmail.com>
2020-04-02 23:27:51 +0300

    LocaleController added

---
+++ src/main/java/ua/iamiluxa/timetrackingspringproject/controller/LocaleController.java
@@ -0,0 +1,31 @@
+package ua.iamiluxa.timetrackingspringproject.controller;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class LocaleController implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.UK);
+        return sessionLocaleResolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setCacheSeconds(100);
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setBasename("classpath:messages");
+        return messageSource;
+    }
+}
